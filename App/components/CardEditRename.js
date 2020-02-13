a94a78bf8a5c6386d9b7221de36eb1c8516894c2
Patrick <29860751+patrick-11@users.noreply.github.com>
2020-02-13 19:06:10 +0100

    Disabled button if nothing entered

--- App/components/CardEditRename.js
+++ App/components/CardEditRename.js
@@ -7,17 +7,12 @@ const CardEditRename = (props) => {
     const [newDeckName, setNewDeckName] = React.useState("");
 
     props.navigation.setOptions({
-        headerRight: () => <Button title = "Save" onPress = {() => {deckRename()}}/>
+        headerRight: () => <Button title = "Save" disabled = {!newDeckName} onPress = {() => {deckRename()}}/>
     });
 
     const deckRename = () => {
-        if(newDeckName.length > 0) {
-            props.container.renameDeck(props.deckName, newDeckName);
-            props.navigation.navigate("Decks");
-        }
-        else {
-            alert("Enter a Deck name!")
-        }
+        props.container.renameDeck(props.deckName, newDeckName);
+        props.navigation.navigate("Decks");
     }
 
     return (
